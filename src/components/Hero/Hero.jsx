4283b59a451b481cb1f3e18499de502e820eaa11
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-09-25 18:08:56 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -91,7 +91,7 @@ const Hero = () => {
                 >
                     <div className="wrapper3">
                             <video autoPlay loop muted className="background-video">
-                                <source src="path_to_your_video.mp4" type="video/mp4" />
+                                <source src="./background1.mp4" type="video/mp4" />
                                 Your browser does not support the video tag.
                             </video>
                         <h1 className="animated-text">VARUN NLV</h1>
