2da44601f59f01c9a3b308a6a79a16182eef7f28
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-09-26 12:55:24 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -61,8 +61,8 @@ const Hero = () => {
                 iteration = 0; // Reset iteration for continuous loop
             }
 
-            iteration += 1 / 10;
-        }, 50);
+            iteration += 1 / 20;
+        }, 100);
 
         return interval; // Return the interval ID for cleanup
     };
