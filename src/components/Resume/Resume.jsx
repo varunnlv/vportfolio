7713219d30736f23ed33b0da07a945341b7a121e
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-10-01 17:27:07 +0530

    Update Resume.jsx

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -16,6 +16,31 @@ const variants = {
     },
 };
 
+
+
+const textVariants = {
+    initial: {
+        x: -300,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        },
+    },
+    // scrollButton: {
+    //     opacity: 0,
+    //     y: 10,
+    //     transition: {
+    //         duration: 2,
+    //         repeat: Infinity,
+    //     },
+    // },
+};
+
 const Resume = () => {
     const ref = useRef();
 
@@ -25,9 +50,9 @@ const Resume = () => {
             variants={variants}
             initial="initial"
             ref={ref}
-            animate={"animate"}
+            whileInView="animate"
         >
-            <motion.div className="titleContainer1" variants={variants}>
+            <motion.div className="titleContainer1" variants={textVariants}>
                 <div className="title">
                     <h2>
                         <motion.b whileHover={{ color: "orange" }}>CERTIFICATIONS</motion.b>
