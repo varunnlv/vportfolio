2e07fd30dc37a2ea6760f3179f2ab5c3e82b1bbe
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-10-01 11:28:08 +0530

    Update parallax.jsx

--- src/components/parallax/parallax.jsx
+++ src/components/parallax/parallax.jsx
@@ -1,11 +1,23 @@
 import { useRef, useState, useEffect } from "react";
 import "./parallax.scss"; // Ensure this includes your CSS animations
-import { motion } from "framer-motion";
+import { motion} from "framer-motion";
 
 const Parallax = ({ type }) => {
     const ref = useRef();
     const [currentText, setCurrentText] = useState("Experience");
     const [animateText, setAnimateText] = useState(false); // State to trigger CSS animation
+
+    function downloadPDF() {
+        // Create an invisible anchor element
+        const a = document.createElement('a');
+        a.href = './Resume1.pdf'; // Replace with the actual path to your PDF
+        a.download = 'Resume.pdf'; // Default name for the downloaded file
+        
+        // Trigger the click event on the anchor
+        a.click();
+      }
+      
+    
 
     useEffect(() => {
         // Function to handle text change with delay
@@ -65,30 +77,37 @@ const Parallax = ({ type }) => {
             </motion.div>
 
             <div className="Righttab1">
-                {/* Your Experience Content */}
-                <div className="experience-item">
+
+                <button className="center-button" onClick={downloadPDF}>Download Resume</button>
+
+                <img className="center-image" alt="" src="./Resume.png" />
+
+                
+                {/* <div className="experience-item">
                     <h3 className="font-bold text-2xl mb-6"  style={{display: 'flex', alignItems: 'center', marginBottom: '10px', marginTop: '20px' }}>May 2022 - Present</h3>
-                    <p className="font-light leading-7 mb-6" style={{display: 'flex', marginBottom: '5px', marginTop: '5px' }}>Senior Software Engineer</p>
+                    <p className="font-light leading-7 mb-6" style={{display: 'flex', marginBottom: '5px', marginTop: '5px' }}>Software Engineer</p>
                     <p className="font-light leading-7 mb-6" style={{display: 'flex', marginBottom: '5px', marginTop: '5px' }}>
                         INTERNATIONAL DESIGN AND ENGINEERING SOLUTIONS PVT.LTD, PUNE
                     </p>
-{/*                     <ul className="font-light leading-7 mb-6 list-disc list-inside">
+                    <ul className="font-light leading-7 mb-6 list-disc list-inside">
                         <li>Worked as a Senior Software Engineer, coordinating team efforts, managing projects, and utilizing Git, GitHub, and GitHub Actions for version control and CI/CD pipelines, and agile project management.</li>
                         <li>Developed standalone applications for the connection of cold-formed steel members using Java, JavaFX and few projects using C# and MYSQL database management.</li>
                         <li>Gained expertise in web development with a focus on HTML, CSS, JavaScript, and React for frontend, and Java with Spring Boot for backend, utilizing AWS RDS for database management.</li>
                         <li>Designed reusable and reliable code for use within distributed cloud environments.</li>
-                    </ul> */}
+                    </ul>
                 </div>
-                {/* Additional Experience Content */}
+      
                 <div className="experience-item">
                     <h3 className="font-bold text-2xl mb-6" style={{display: 'flex', alignItems: 'center', marginBottom: '10px', marginTop: '20px' }}>JAN 2022 - APR 2022</h3>
                     <p className="font-light leading-7 mb-6" style={{display: 'flex', marginBottom: '5px', marginTop: '5px' }}>Software Developer Intern</p>
                     <p className="font-light leading-7 mb-6" style={{display: 'flex', marginBottom: '5px', marginTop: '5px' }}>EON Designers, Hyderabad</p>
-{/*                     <ul className="font-light leading-7 mb-6 list-disc list-inside">
+                    <ul className="font-light leading-7 mb-6 list-disc list-inside">
                         <li>Developed automation of shear wall design, Estimation, and Detailing project using Python programming language.</li>
                         <li>Learned about some python predefined modules i.e., ezdxf, Docx, Openpyxl.</li>
-                    </ul> */}
-                </div>
+                    </ul>
+                </div> */}
+
+
             </div>
 
             <motion.div className="mountains"></motion.div>
